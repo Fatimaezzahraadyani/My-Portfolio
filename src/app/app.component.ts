752efe4e959697bc364c9e40410b3d51e4980ebc
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-04-09 17:58:56 +0200

    resum component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterPortfolioComponent } from './footer-portfolio/footer-portfolio.component';
 import { HomePortfolioComponent } from './home-portfolio/home-portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterPortfolioComponent,HomePortfolioComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterPortfolioComponent,HomePortfolioComponent,AboutMeComponent,ResumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
