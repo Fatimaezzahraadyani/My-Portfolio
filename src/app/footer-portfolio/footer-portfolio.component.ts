9383f6f879f1f95aed562a509ea0a67add50ab47
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-04-09 10:40:34 +0200

    first commit

---
+++ src/app/footer-portfolio/footer-portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-portfolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-portfolio.component.html',
+  styleUrl: './footer-portfolio.component.css'
+})
+export class FooterPortfolioComponent {
+
+}
